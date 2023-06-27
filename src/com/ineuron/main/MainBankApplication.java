39db1e1d8c6d77cb172a4d6a91cbd51fcb904479
siddharthmone7 <siddharthmone7@gmail.com>
2023-06-27 18:54:49 +0530

    initial commit to create base classes

--- src/com/ineuron/main/MainBankApplication.java
+++ src/com/ineuron/main/MainBankApplication.java
@@ -1,10 +1,11 @@
 package com.ineuron.main;
 
-public class MainBankApplication {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class MainBankApplication 
+{
+	public static void main(String[] agrs)
+	{
+		
 	}
-
+	
 }
